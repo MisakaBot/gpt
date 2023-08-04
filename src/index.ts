671e3abd6c86966bd3b31a726d84b1aeff009ae5
WitchElaina <msz13399173387@gmail.com>
2023-08-04 09:28:56 +0000

    first commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { GPT } from './gpt';
+
+import { Context, Schema } from 'koishi';
+
+export const name = 'gpt';
+
+export interface Config {
+  apiKey: string;
+  model?: string;
+  httpProxy?: string;
+}
+
+export const Config: Schema<Config> = Schema.object({
+  apiKey: Schema.string().required(),
+  model: Schema.string(),
+  httpProxy: Schema.string(),
+});
+
+export function apply(ctx: Context, config: Config) {
+  const gpt = new GPT({
+    apiKey: config.apiKey,
+    httpProxy: config.httpProxy,
+    model: config.model,
+  });
+  ctx
+    .command('gpt.chat <content>', 'Send message to gpt.')
+    .alias('gc')
+    .action(async ({ session }, content) => {
+      const res = await gpt.newChat(content);
+      return res;
+    });
+  ctx.command('gpt.info').action(() => {
+    return gpt.getInfo();
+  });
+  ctx.command('gpt.clear').action(() => {
+    gpt.clearChat();
+    return 'Cleared!';
+  });
+  ctx.command('gpt.init <content>').action((_, content) => {
+    gpt.initial = content;
+    return 'Init success!';
+  });
+}
